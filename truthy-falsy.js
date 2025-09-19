f7526c8d36b0c6ab286588134458cf01d954c745
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-09-20 00:30:45 +0600

    truthy and falsy value

---
+++ truthy-falsy.js
@@ -0,0 +1,12 @@
+//there are total 8 falsy value available on javascript
+
+//------****--------//
+
+//* false
+//* 0
+//* -0
+//* 0n (BigInt Zero)
+//* "" (empty string)
+//* null
+//* undefined
+//* NaN
